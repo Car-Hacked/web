f06d94e95513505fbb42395ea38951dc46789fb1
Tim Gass <timothyagass@gmail.com>
2026-04-14 13:08:44 -0500

    fixed incorrect url

--- src/js/main.js
+++ src/js/main.js
@@ -19,7 +19,7 @@ const Main = () => {
       }
       setImageSrc(Buffer.from(frame).toString('base64'));
     });
-    var socket2 = io('https://catracker-api.fly.dev');
+    var socket2 = io('https://cartracker-api.fly.dev');
     socket2.on('updated', (data) => {
       if (data.garageName === 'Johnson') {
         setGarage({
@@ -29,7 +29,7 @@ const Main = () => {
         });
       }
     });
-    $.get('https://catracker-api.fly.dev/api/v1/garages/63f95e3d12e22127cda94f78', response => {
+    $.get('https://cartracker-api.fly.dev/api/v1/garages/63f95e3d12e22127cda94f78', response => {
         if (response instanceof Error) {
           return;
         }
